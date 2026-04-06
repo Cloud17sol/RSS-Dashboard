35f0faf68c1fee6bcb5334b80693a4170239bcb6
Cloud17sol <osfilani@gmail.com>
2026-04-06 17:03:56 +0100

    Light mode fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Must match useRSSFeeds `applyTheme`: toggle `dark` on <html>, not OS media only.
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
